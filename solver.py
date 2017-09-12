f4591a5f8cee868b742f9c87c3f8407b29fa70c7
Odin Blomhoff Pedersen <odinblomhoff@gmail.com>
2017-09-12 16:07:27 +0200

    .

--- solver.py
+++ solver.py
@@ -4,3 +4,14 @@
 #Får tilbake Goal-node der den har kommet til mål
 #Backtracke fra goal-node for å finne path
 #Backtack ferdig til parent node = start node
+
+import Vehicle
+
+E = [[0,2,2,2],
+[0,0,4,3],
+[0,3,4,2],
+[0,4,1,2],
+[1,2,0,2],
+[1,4,2,2]]
+
+vehicle = 

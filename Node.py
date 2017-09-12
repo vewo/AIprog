88416813ed5906738df49b554fef70cf193b0af1
Odin Blomhoff Pedersen <odinblomhoff@gmail.com>
2017-09-12 18:22:34 +0200

    updated node

--- Node.py
+++ Node.py
@@ -4,7 +4,7 @@ class Node(object):
 	g_value = 0
 	h_value = 0
 	f_value = g_value + h_value
-	#status = Open
+	status = Open
 	parent = None
 	kids = []
 
@@ -15,9 +15,7 @@ class Node(object):
 
 
 	def heuristic(self, b):
-	    (x1, y1) = a
-	    (x2, y2) = b
-	    #return abs(x1 - x2) + abs(y1 - y2)
+	    goalDistance = 4-self.state.vehicles[0].x
 	    return math.sqrt((x1 - x2)**2) + math.sqrt((y1 - y2)**2) #Manhattan distance
 
 	def getID(self):
@@ -26,7 +24,38 @@ class Node(object):
 	def generateChildren(self):
 		children = []
 		for v in self.state.vechicles:
-			break
+			if v.orientation = 0:
+				if (self.state.isAvailable(v.x-1, v.y)):
+					kidState = self.state.vehicles
+					kidState.remove(v)
+					newCarPosition = Vehicle(v.no, v.orientation, v.x-1, v.y, v.size)
+					kidState.insert(v.no, newCarPosition)
+					kid = Node(kidState, self)
+					children.append(kid)
+				if (self.state.isAvailable(v.x+v.size, v.y)):
+					kidState = self.state.vehicles
+					kidState.remove(v)
+					newCarPosition = Vehicle(v.no, v.orientation, v.x+1, v.y, v.size)
+					kidState.insert(v.no, newCarPosition)
+					kid = Node(kidState, self)
+					children.append(kid)
+			elif v.orientation = 1:
+				if (self.state.isAvailable(v.x, v.y+1)):
+					kidState = self.state.vehicles
+					kidState.remove(v)
+					newCarPosition = Vehicle(v.no, v.orientation, v.x, v.y-1, v.size)
+					kidState.insert(v.no, newCarPosition)
+					kid = Node(kidState, self)
+					children.append(kid)
+				if (self.state.isAvailable(v.x, v.y+size)):
+					kidState = self.state.vehicles
+					kidState.remove(v)
+					newCarPosition = Vehicle(v.no, v.orientation, v.x, v.y+1, v.size)
+					kidState.insert(v.no, newCarPosition)
+					kid = Node(kidState, self)
+					children.append(kid)
+		return children
+
 
 	def check_Solution(self):
 		return (((self.state.vechicles[0].x + self.state.vechicles[0].size - 1) == 5) and (self.state.vechicles[0].y == 2))
@@ -40,24 +69,32 @@ class State(object):
 
 	vehicles = []
 
-	def __init__(self):
-	    self.no = no
-	    self.orientation = orientation
-	    self.x = x
-	    self.y = y
+	def __init__(self, vehicles):
+	    self.vehicles = vehicles
 
 	def hashID(self):
 		hashID = 0
 		for v in vehicles:
 			hashID += (3*v.no + 13*v.x + 17*v.y + 107*size) #Is this really a good hash??
 
+	def isAvailable(x,y):
+		for v in self.vehicles:
+			if v.orientation = 0:
+				if (v.y = y) and (x in range(v.x, v.x+size)):
+					return False
+			elif v.orientation = 1:
+				if (v.x = x) and (y in range(v.y, v.y+size)):
+					return False
+			return True
+
+
 
 
 class Vehicle(object):
 
-	def __init__(self, orientation, x,y,size, no):
-		self.orientation = orientation
-		self.x = x
-		self.y = y
-		self.size = size
-		self.no = no
+	def __init__(self, no, orientation, x,y,size):
+	    self.no = no
+	    self.orientation = orientation
+	    self.x = x
+	    self.y = y
+	    self.size = size

241b780e3d5e336c1d196996681d1130ca937372
vewo <vemundwo@gmail.com>
2017-09-12 10:12:11 +0200

    Initial code

---
+++ Node.py
@@ -0,0 +1,62 @@
+
+class Node(object):
+
+	g_value = 0
+	h_value = 0
+	f_value = g_value + h_value
+	status = Open
+	parent = None
+	kids = []
+
+	def __init__(self, state, parent):
+	    self.state = state
+	    self.parent = parent
+	    self.walls = [(4,3),(4,4)]
+
+
+	def heuristic(self, b):
+	    (x1, y1) = a
+	    (x2, y2) = b
+	    #return abs(x1 - x2) + abs(y1 - y2)
+	    return math.sqrt((x1 - x2)**2) + math.sqrt((y1 - y2)**2) #Manhattan distance
+
+	def getID(self):
+		return self.state.hashID()
+
+	def generateChildren(self): 
+		children = []
+		for v in self.state.vechicles:
+
+	def check_Solution(self):
+		return (((self.state.vechicles[0].x + self.state.vechicles[0].size - 1) == 5) and (self.state.vechicles[0].y == 2))
+
+	def cost(a,b):
+		return 1
+
+
+
+class State(object):
+
+	vehicles = []
+
+	def __init__(self):
+	    self.no = no
+	    self.orientation = orientation
+	    self.x = x
+	    self.y = y
+
+	def hashID(self):
+		hashID = 0
+		for v in vehicles:
+			hashID += (3*v.no + 13*v.x + 17*v.y + 107*size) #Is this really a good hash??
+
+
+
+class Vehicle(object):
+
+	def __init__(self, no, orientation, x,y,size):
+	    self.no = no
+	    self.orientation = orientation
+	    self.x = x
+	    self.y = y
+	    self.size = size
---
+++ astar.py
@@ -0,0 +1,61 @@
+from queue import PriorityQueue
+import math
+import Node
+import State
+import Vehicle
+
+class A_star_search(object): 
+
+	def a_star_search(graph, start):
+		closed = []
+		frontier = PriorityQueue() #Nodes
+		created = {} #(hashID, nodes)
+
+		frontier.put(((0+start.heuristic(start, goal)), start)) #g(S0)=0
+
+		while True: 
+			if frontier.empty():
+				return False
+			x = frontier.get()[1]
+			closed.add(x)
+			if x.check_Solution():
+				return x
+			children = x.generateChildren()
+			for c in children:
+				if created.hasKey(c.getID()):
+					c = created[created.get(c.getID())] #Probably not right
+				else: 
+					created.add(c.getID(), c)
+				x.kids.add(c)
+				if not (c in closed or c in list(frontier)):
+					f = attach_and_eval(c,x)
+					frontier.put((f, c))
+				else if x.g_value + cost(x,c) < c.g_value():
+					attach_and_eval(c,x)
+					if c in closed:
+						propagate_path_improvements(c)
+
+	def attach_and_eval(self, a,b):
+		a.parent = b
+		a.g_value = b.g_value + b.cost(a,b)
+		a.f_value = a.g_value + a.heuristic(b)
+		return a.f_value
+
+	def propagate_path_improvements(self, a):
+		for c in a.kids:
+			if a.g_value + cost(a,c) < c.g_value:
+				c.parent = a
+				c.g_value = a.g_value + cost(a,c)
+				c.f_value = c.g_value + a.heuristic(a)
+				propagate_path_improvements(c)
+
+
+
+
+
+
+
+
+
+
+

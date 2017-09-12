ada0ea07c1a93f0e108b9415b9f67e052825a651
Odin Blomhoff Pedersen <odinblomhoff@gmail.com>
2017-09-12 10:25:27 +0200

    Todo

---
+++ solver.py
@@ -0,0 +1,6 @@
+#Importere data og gjøre om til vehicle object
+#Lage start state med vehicle object
+#Gi start state til Astar
+#Får tilbake Goal-node der den har kommet til mål
+#Backtracke fra goal-node for å finne path
+#Backtack ferdig til parent node = start node

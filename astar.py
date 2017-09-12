7cbc01f57a2d7a3beeedaaedf9c8ea8ba3901b68
Odin Blomhoff Pedersen <odinblomhoff@gmail.com>
2017-09-12 18:23:14 +0200

    updated astar

--- astar.py
+++ astar.py
@@ -4,16 +4,18 @@ import Node
 import State
 import Vehicle
 
-class A_star_search(object): 
+class A_star_search(object):
 
 	def a_star_search(graph, start):
 		closed = []
+		open_nodes = []
 		frontier = PriorityQueue() #Nodes
 		created = {} #(hashID, nodes)
 
 		frontier.put(((0+start.heuristic(start, goal)), start)) #g(S0)=0
+		open_nodes.append(start)
 
-		while True: 
+		while True:
 			if frontier.empty():
 				return False
 			x = frontier.get()[1]
@@ -24,15 +26,22 @@ class A_star_search(object):
 			for c in children:
 				if created.hasKey(c.getID()):
 					c = created[created.get(c.getID())] #Probably not right
-				else: 
+				else:
 					created.add(c.getID(), c)
 				x.kids.add(c)
-				if not (c in closed or c in list(frontier)):
+				if not ((c in closed) or (c in open_nodes)):
 					f = attach_and_eval(c,x)
 					frontier.put((f, c))
-				else if x.g_value + cost(x,c) < c.g_value():
+					open_nodes.append(c)
+				elif x.g_value + x.cost(x,c) < c.g_value():
 					attach_and_eval(c,x)
-					if c in closed:
+					if c in open_nodes:
+						open_nodes_temp = []
+						for i in range(len(open_nodes)):
+							open_nodes_temp.append(frontier.get()[1])
+						for i in range(len(open_nodes_temp)):
+							frontier.put((open_nodes_temp[i].f_value, open_nodes_temp[i]))
+					elif c in closed:
 						propagate_path_improvements(c)
 
 	def attach_and_eval(self, a,b):
@@ -48,14 +57,3 @@ class A_star_search(object):
 				c.g_value = a.g_value + cost(a,c)
 				c.f_value = c.g_value + a.heuristic(a)
 				propagate_path_improvements(c)
-
-
-
-
-
-
-
-
-
-
-
